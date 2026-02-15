7acdf7980123e0f6e853c8173602ddefc4df3610
Ayush Gaikwad <ayushgaikwad0605@gmail.com>
2026-02-15 14:43:58 +0530

    Update URL

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://farmiq-prototype.vercel.app"
+        "https://farmiq-prototype.vercel.app" 
     ],
     allow_credentials=True,
     allow_methods=["*"],
